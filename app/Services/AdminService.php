dc1a0e52ebd8390f51eca1ce23cf73786b6fb4df
Emmanuel Oteng Wilson <hagioswilson@gmail.com>
2022-02-11 20:52:05 +0000

    modified admin service and added exceptions

--- app/Services/AdminService.php
+++ app/Services/AdminService.php
@@ -41,6 +41,8 @@ class AdminService extends ManageAccountService
         return new AdminResource($admin);
     }
 
+
+
     public function updateAdmin(Admin $admin, NewAdminRequest $request): JsonResponse
     {
         $admin->update($request->except('role'));
