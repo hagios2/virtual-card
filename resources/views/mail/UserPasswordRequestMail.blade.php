d1d4069a8a90dfac0bb8b1cf12c3ce000099e060
Emmanuel Oteng Wilson <hagioswilson@gmail.com>
2022-02-11 11:49:02 +0000

    finished added password reset mails

--- resources/views/mail/UserPasswordRequestMail.blade.php
+++ resources/views/mail/UserPasswordRequestMail.blade.php
@@ -1,11 +1,27 @@
 @component('mail::message')
-# Introduction
+# Hello {{$user->name}}
 
-The body of your message.
+<section>
+    <p>
+        You have requested to reset your password, kindly click on the button below to reset your password.
+    </p>
+</section>
 
-@component('mail::button', ['url' => ''])
-Button Text
+@component('mail::button', ['url' => env('PASSWORD_RESET_URL')."?token={$passwordReset->token}"])
+    Reset Password
 @endcomponent
+
+<section>
+    <p>
+        or you may copy and paste the link below in your browser to reset your password
+    </p>
+
+    <a href="{{env('PASSWORD_RESET_URL')."?token={$passwordReset->token}"}}" target="_blank">{{env('PASSWORD_RESET_URL')."?token={$passwordReset->token}"}}</a>
+</section>
+
+<section>
+    <p> Please ignore this mail if you didn't perform this operation.</p>
+</section>
 
 Thanks,<br>
 {{ config('app.name') }}
