8a3bf6be2928b41b035218489af1bc2af1b60f64
Emmanuel Oteng Wilson <hagioswilson@gmail.com>
2022-02-14 19:51:10 +0000

    modified response

--- app/Http/Requests/AgencyRegistrationRequest.php
+++ app/Http/Requests/AgencyRegistrationRequest.php
@@ -4,7 +4,6 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
-use JetBrains\PhpStorm\ArrayShape;
 
 class AgencyRegistrationRequest extends FormRequest
 {
