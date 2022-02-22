41f9c6f9ed18ee81cfc893ab8eabc5dd0b01cea3
Emmanuel Oteng Wilson <hagioswilson@gmail.com>
2022-02-22 05:26:21 +0000

    removed unused imports

--- app/Services/UserAccountService.php
+++ app/Services/UserAccountService.php
@@ -9,10 +9,8 @@ use App\Mail\ResendVerificationLinkMail;
 use App\Mail\UserRegistrationMail;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Str;
 
 class UserAccountService
 {
