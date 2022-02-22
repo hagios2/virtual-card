8c8192d942e4965fbbf0065f4cbd6c2dc6219740
Emmanuel Oteng Wilson <hagioswilson@gmail.com>
2022-02-22 17:24:43 +0000

    fixed errors in migration

--- database/migrations/2022_02_16_165542_create_subscriptions_table.php
+++ database/migrations/2022_02_16_165542_create_subscriptions_table.php
@@ -17,7 +17,7 @@ class CreateSubscriptionsTable extends Migration
         Schema::create('subscriptions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users');
-            $table->foreignId('agency_id')->nullable()->constrained('agen');
+            $table->foreignId('agency_id')->nullable()->constrained('agency');
             $table->string('status')->default(Subscription::PENDING_STATUS);
             $table->string('type'); //standard / private
             $table->timestamp('paid_at')->nullable();
