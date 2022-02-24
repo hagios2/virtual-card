00b332df13eb3943c85e4a062e1445e30d3e04a8
Emmanuel Oteng Wilson <hagioswilson@gmail.com>
2022-02-24 22:28:06 +0000

    modified middleware roles

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -15,7 +15,7 @@ class AdminController extends Controller
 {
     public function __construct(protected AdminService $adminService)
     {
-        $this->middleware(['auth:admin', 'role:admin']);
+        $this->middleware(['auth:admin', 'role:admin,super admin']);
     }
 
     public function createAdmin(NewAdminRequest $request): JsonResponse
