a705bbfffd12e4f9e6a89ec61796715490717880
Emmanuel Oteng Wilson <hagioswilson@gmail.com>
2022-03-04 09:57:44 +0000

    modifying service charge  migration

---
+++ database/migrations/2022_03_04_094759_add_changes_to_service_charge_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddChangesToServiceChargeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('service_charges', function (Blueprint $table) {
+            $table->dropColumn('private_security_fee');
+            $table->renameColumn('standard_subscription_fee', 'amount');
+            $table->string('type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('service_charges', function (Blueprint $table) {
+            $table->decimal('private_security_fee', 10, 2);
+            $table->renameColumn('amount', 'standard_subscription_fee');
+            $table->dropColumn('type');
+        });
+    }
+}
