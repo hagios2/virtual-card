bfc8c3d5be8c356b04c1e8f730e452c6a4d66a07
Emmanuel Oteng Wilson <hagioswilson@gmail.com>
2022-03-06 19:44:07 +0000

    made changes

--- app/Http/Controllers/User/AuthController.php
+++ app/Http/Controllers/User/AuthController.php
@@ -15,7 +15,7 @@ class AuthController extends Controller
 {
     public function __construct(protected UserAuthService $userAuthService)
     {
-        $this->middleware('auth:api', ['except' => ['login']]);
+        $this->middleware('auth:api', ['except' => ['login', 'sendResetMail', 'resetPassword']]);
     }
 
     public function login(LoginRequest $request): JsonResponse
